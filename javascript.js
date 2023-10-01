1255c435cd8b6a8b1193dc965041f7bf8780b2c1
Bluecode77732 <doookuuu@gmail.com>
2023-10-01 17:01:58 +0900

    First Commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/* I am having a accessing on array and any other methods in JS. Therefore, I write in here the codes I don't understood or forget, and some of methods made me noticed. */
+
+/* concat */
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.concat() //"Mango"
+
+
+const arr1 = ["Emil", "Tobias", "Linus"];
+const myChildren = arr1.concat("Peter"); //["Emil", "Tobias", "Linus", "Peter"]
+
+
+
+
+/*  */
+
+
+
